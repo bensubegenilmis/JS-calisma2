d5279ca5339f10a06c08f77f3e62b328a6a6bdb3
Bensu Beğenilmiş <56439963+bensubegenilmis@users.noreply.github.com>
2024-04-10 00:48:26 +0300

    last

--- sort-method.js
+++ sort-method.js
@@ -14,3 +14,20 @@ const compareFunction = (a,b)=> {
 };
 
 console.log(nums.sort(compareFunction));
+
+const people =[
+    {name: "Bensu", age: 25},
+    {name: "Duygu", age: 22},
+    {name: "Veli", age: 30},
+    {name: "Ali", age: 28},
+];
+
+const compareFunctionObj = (a,b)=> {
+
+    //1.  <0 => a comes first
+    //2. 0 => nothing changes
+    //3. >0 b comes first
+    return a.age - b.age;
+};
+
+console.log(people.sort(compareFunctionObj));
